b8dbd08f195ccad7066ebe32198fb9d6f4e00711
usama99 <31574039+usama99@users.noreply.github.com>
2019-08-21 13:47:54 +0500

    Initial commit

---
+++ app/src/main/java/com/example/usama/importpush/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.usama.importpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testUpload(){
+        int i = 1 + 1;
+    }
+}
